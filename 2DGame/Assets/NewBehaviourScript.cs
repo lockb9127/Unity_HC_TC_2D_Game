e694faa3db50d4465232d20ef51a7d3037f860df
lockb9127 <66513347+lockb9127@users.noreply.github.com>
2020-12-08 21:30:45 +0800

    認識程式語言與設計遊戲語法

---
+++ 2DGame/Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("這是汽車"), Range(0, 150)]
+    public int cat = 100;
+    private void Start()
+    {
+        print("你好");
+        print(cat);
+
+    }
+
+
+}
---
+++ 2DGame/Assets/TetrisManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+    [Header("掉落時間"), Range(0.1f, 10)]
+    public float time = 1.55f;
+    [Header("目前分數")]
+    public int score = 1;
+    [Header("最高分數")]
+    public int scoreA = 1;
+    [Header("等級")]
+    public int Lv = 1;
+    [Header("存放物件")]
+    public GameObject Object;
+    [Header("掉落音效")]
+    public AudioClip SpriteA;
+    [Header("選轉音效")]
+    public AudioClip SpriteB;
+    [Header("消除音效")]
+    public AudioClip SpriteC;
+    [Header("遊戲結束音效")]
+    public AudioClip SpriteD;
+
+
+
+}
