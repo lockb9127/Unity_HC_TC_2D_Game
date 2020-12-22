021f476a7c81aaedc4556ed755aa3c6f6c71a1d8
lockb9127 <66513347+lockb9127@users.noreply.github.com>
2020-12-22 21:32:59 +0800

    移動腳本

--- 2DGame/Assets/腳本/TetrisManager.cs
+++ 2DGame/Assets/腳本/TetrisManager.cs
@@ -7,25 +7,59 @@ public class TetrisManager : MonoBehaviour
     [Header("目前分數")]
     public int score = 1;
     [Header("最高分數")]
-    public int scoreA = 1;
+    public int scoreA = 99;
     [Header("等級")]
     public int Lv = 1;
     [Header("存放物件")]
     public GameObject Object;
     [Header("掉落音效")]
-    public AudioClip SpriteA;
+    public AudioClip down;
     [Header("選轉音效")]
     public AudioClip SpriteB;
     [Header("消除音效")]
     public AudioClip SpriteC;
     [Header("遊戲結束音效")]
     public AudioClip SpriteD;
-    
+    [Header("下一個俄羅斯方塊區域")]
+    public Transform box;
+    [Header("畫布")]
+    public Transform Canvas;
+
+    public RectTransform currentTetris;
+
+    public int indexNext;
+    private void Start()
+    {
+        RandomTetris();
+    }
+
+    public float timer;
+    private void controlTertis()
+    {
+        time += Time.deltaTime;
+    }
     private void generate()
     {
 
     }
+    public void RandomTetris()
+    {
+        indexNext = Random.Range(0, 7);
+        traNextAree.GetChild(indexNext).gameObject.SetActive(true);
+    }
 
+    public void ControlTertis()
+    {
+        if(currentTetris)
+        {
+            time += time.deltaTime;
+            if(time>=timeFall)
+            {
+                timer = 0;
+                currentTetris.anchoredPosition -= new Vector2(0, 50);
+            }
+        }
+    }
     private void score01()
     {
 
